ec803beef0ddae086738ce6410c0516594add514
Bloemenstraat <junge.jager@gmail.com>
2022-05-18 16:24:23 +0100

    Oups

--- routes/biens.js
+++ routes/biens.js
@@ -46,8 +46,20 @@ const biens = [
 }
 ];
 
-biensRouter.use('/', (req, res) => {
-    res.status(200).send(biens);
+biensRouter.use('/rent', (req, res) => {
+    const rent = biens.filter((e) => {
+        return e.type == 'location';
+    });
+
+    res.status(200).send(rent);
+});
+
+biensRouter.use('/buy', (req, res) => {
+    const vente = biens.filter((e) => {
+        return e.type == 'vente';
+    });
+
+    res.status(200).send(vente);
 });
 
 module.exports = biensRouter;
